1f6738b76ca17ed092c64978b0d358e87c18fc5e
Jordan BRION <jrd.brion@gmail.com>
2020-02-22 11:10:47 +0100

    main.rs: write to .ppm

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,44 @@
+mod ppm;
+
+use ppm::*;
+
+struct Vec3 {
+    x: f32,
+    y: f32,
+    z: f32,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let v1 = Vec3 {
+        x: 0.0,
+        y: 0.0,
+        z: 0.0,
+    };
+
+    let nx = 200;
+    let ny = 100;
+    let mut ppm = PPM::new(ny, nx);
+
+    for j in 0..ny {
+        for i in 0..nx {
+            let r = i as f32 / nx as f32;
+            let g = j as f32 / ny as f32;
+            let b = 0.2;
+            let ir = (255.99 * r) as u8;
+            let ig = (255.99 * g) as u8;
+            let ib = (255.99 * b) as u8;
+
+            let final_y = ny - j;
+            ppm.set_pixel(
+                i,
+                final_y,
+                RGB {
+                    r: ir,
+                    g: ig,
+                    b: ib,
+                },
+            );
+        }
+    }
+    ppm.write_file("test.ppm").expect("Cannot write ppm file!!");
 }
