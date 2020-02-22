dca1e919fd97c86df62801bd7c209e78db03964e
Jordan BRION <jrd.brion@gmail.com>
2020-02-22 17:53:54 +0100

    ray.rs: fix module dependency problem

--- src/ray.rs
+++ src/ray.rs
@@ -1,15 +1,13 @@
-mod Vec3;
-
-use Vec3::*;
+use super::vec3::*;
 
 pub struct Ray {
-    origin: Vec3,
-    direction: Vec3
+    pub origin: Vec3,
+    pub direction: Vec3
 }
 
 impl Ray {
 
-    fn point_at_parameter(&self, t: f32) -> Vec3 {
+    pub fn point_at_parameter(&self, t: f32) -> Vec3 {
         self.origin + t * self.direction
     }
 }
