5de2492518a162200beff4246b7681d1fc64bc5c
Jordan BRION <jrd.brion@gmail.com>
2020-02-22 14:32:03 +0100

    main.rs: use Vec3

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,20 @@
 mod ppm;
+mod vec3;
 
 use ppm::*;
-
-struct Vec3 {
-    x: f32,
-    y: f32,
-    z: f32,
-}
+use vec3::*;
 
 fn main() {
-    let v1 = Vec3 {
-        x: 0.0,
-        y: 0.0,
-        z: 0.0,
-    };
-
     let nx = 200;
     let ny = 100;
     let mut ppm = PPM::new(ny, nx);
 
     for j in 0..ny {
         for i in 0..nx {
-            let r = i as f32 / nx as f32;
-            let g = j as f32 / ny as f32;
-            let b = 0.2;
-            let ir = (255.99 * r) as u8;
-            let ig = (255.99 * g) as u8;
-            let ib = (255.99 * b) as u8;
+            let v = Vec3::new(i as f32 / nx as f32, j as f32 / ny as f32, 0.2);
+            let ir = (255.99 * v.r()) as u8;
+            let ig = (255.99 * v.g()) as u8;
+            let ib = (255.99 * v.b()) as u8;
 
             let final_y = ny - j;
             ppm.set_pixel(
