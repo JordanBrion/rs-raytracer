998333ec94062f057d12b9a71fead9d0a4d53b6e
Jordan BRION <jrd.brion@gmail.com>
2020-02-22 17:58:24 +0100

    main.rs: calculate color based on ray direction

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,36 @@
 mod ppm;
+mod ray;
 mod vec3;
 
 use ppm::*;
+use ray::*;
 use vec3::*;
+
+fn color(ray: &Ray) -> Vec3 {
+    let unit_direction = ray.direction.unit_vector();
+    let t = 0.5 * (unit_direction.y + 1.0);
+    (1.0 - t) * Vec3::new(1.0, 1.0, 1.0) + t * Vec3::new(0.5, 0.7, 1.0)
+}
 
 fn main() {
     let mut ppm = PPM::new(100, 200);
-
-
+    let lower_left_corner = Vec3::new(-2.0, -1.0, -1.0);
+    let horizontal = Vec3::new(4.0, 0.0, 0.0);
+    let vertical = Vec3::new(0.0, 2.0, 0.0);
+    let origin = Vec3::new(0.0, 0.0, 0.0);
 
     for j in 0..ppm.height {
         for i in 0..ppm.width {
-            let v = Vec3::new(i as f32 / ppm.width as f32, j as f32 / ppm.height as f32, 0.2);
-            let ir = (255.99 * v.r()) as u8;
-            let ig = (255.99 * v.g()) as u8;
-            let ib = (255.99 * v.b()) as u8;
+            let u = i as f32 / ppm.width as f32;
+            let v = j as f32 / ppm.height as f32;
+            let ray = Ray {
+                origin: origin,
+                direction: lower_left_corner + u * horizontal + v * vertical,
+            };
+            let c = color(&ray);
+            let ir = (255.99 * c.r()) as u8;
+            let ig = (255.99 * c.g()) as u8;
+            let ib = (255.99 * c.b()) as u8;
 
             let final_y = ppm.height - j;
             ppm.set_pixel(
