f32d3a034bd1f52a6d1d5da0865bf096305ac76e
Jordan BRION <jrd.brion@gmail.com>
2021-05-11 15:23:28 +0200

    An Optimized AABB Hit Method

--- src/aabb.rs
+++ src/aabb.rs
@@ -12,14 +12,12 @@ struct AABB {
 impl Hittable for AABB {
     fn hit(&self, ray: &Ray, t_min: f64, t_max: f64) -> Option<HitRecord> {
         for i in 0..3 {
-            let t0 = fmin(
-                self.minimum[i] - ray.origin[i] / ray.direction[i],
-                self.maximum[i] - ray.origin[i] / ray.direction[i],
-            );
-            let t1 = fmax(
-                self.minimum[i] - ray.origin[i] / ray.direction[i],
-                self.maximum[i] - ray.origin[i] / ray.direction[i],
-            );
+            let inv_d = 1.0 / ray.direction[i];
+            let mut t0 = (self.minimum[i] - ray.origin[i]) * inv_d;
+            let mut t1 = (self.maximum[i] - ray.origin[i]) * inv_d;
+            if inv_d < 0.0 {
+                std::mem::swap(&mut t0, &mut t1);
+            }
             let t_min = fmax(t0, t_min);
             let t_max = fmin(t1, t_max);
             if t_max <= t_min {
