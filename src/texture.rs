6a3bb4944b882a25c2ff14982d66dc2363c06cd0
Jordan BRION <jrd.brion@gmail.com>
2021-05-12 21:45:32 +0200

    A Checker Texture

--- src/texture.rs
+++ src/texture.rs
@@ -5,6 +5,11 @@ pub struct SolidColor {
     pub color_value: Color,
 }
 
+pub struct CheckTexture<'a> {
+    odd: &'a dyn Texture,
+    even: &'a dyn Texture,
+}
+
 pub trait Texture {
     fn value(&self, u: f64, v: f64, point: &Vec3) -> Color;
 }
@@ -12,6 +17,18 @@ pub trait Texture {
 impl Texture for SolidColor {
     fn value(&self, u: f64, v: f64, point: &Vec3) -> Color {
         self.color_value
+    }
+}
+
+impl<'a> Texture for CheckTexture<'a> {
+    fn value(&self, u: f64, v: f64, point: &Vec3) -> Color {
+        let factor = 10.0;
+        let sines = (factor * point.x).sin() + (factor * point.y).sin() * (factor * point.z).sin();
+        if sines < 0.0 {
+            self.odd.value(u, v, point)
+        } else {
+            self.even.value(u, v, point)
+        }
     }
 }
 
