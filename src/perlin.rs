6b31a3f403f90da77c4b49d6b8fe490bc15b281a
Jordan BRION <jrd.brion@gmail.com>
2021-05-13 18:03:05 +0200

    Using Random Vectors on the Lattice Points

--- src/perlin.rs
+++ src/perlin.rs
@@ -4,7 +4,7 @@ use super::vec3::*;
 const POINT_COUNT: usize = 256;
 
 pub struct Perlin {
-    ranfloat: Vec<f64>,
+    ranvec: Vec<Vec3>,
     perm_x: Vec<usize>,
     perm_y: Vec<usize>,
     perm_z: Vec<usize>,
@@ -12,12 +12,12 @@ pub struct Perlin {
 
 impl Perlin {
     pub fn new() -> Perlin {
-        let mut ranfloat = Vec::with_capacity(POINT_COUNT);
+        let mut ranvec = Vec::with_capacity(POINT_COUNT);
         for _ in 0..POINT_COUNT {
-            ranfloat.push(random_double());
+            ranvec.push(Vec3::random_in_limit(-1.0, 1.0).unit_vector());
         }
         Perlin {
-            ranfloat: ranfloat,
+            ranvec: ranvec,
             perm_x: Self::perlin_generate_perm(),
             perm_y: Self::perlin_generate_perm(),
             perm_z: Self::perlin_generate_perm(),
@@ -28,12 +28,9 @@ impl Perlin {
         let u = p.x - p.x.floor();
         let v = p.y - p.y.floor();
         let w = p.z - p.z.floor();
-        let uu = u * u * (3.0 - 2.0 * u);
-        let vv = v * v * (3.0 - 2.0 * v);
-        let ww = w * w * (3.0 - 2.0 * w);
-        let i = uu as i64;
-        let j = vv as i64;
-        let k = ww as i64;
+        let i = u as i64;
+        let j = v as i64;
+        let k = w as i64;
         let mut c = Vec::with_capacity(8);
         let mask = 255;
         let max = 2;
@@ -41,16 +38,17 @@ impl Perlin {
             for dj in 0..max {
                 for dk in 0..max {
                     c.push(
-                        self.ranfloat[self.perm_x[((i + di) & mask) as usize]
+                        self.ranvec[self.perm_x[((i + di) & mask) as usize]
                             ^ self.perm_y[((j + dj) & mask) as usize]
                             ^ self.perm_z[((k + dk) & mask) as usize]],
                     );
                 }
             }
         }
-        Self::trilinear_interp(&c, uu, vv, ww)
+        Self::perlin_interp(&c, u, v, w)
     }
 
+    #[allow(dead_code)]
     fn trilinear_interp(c: &[f64], u: f64, v: f64, w: f64) -> f64 {
         let mut accum = 0.0;
         let max = 2;
@@ -65,6 +63,31 @@ impl Perlin {
                         * (f_j * v + (1.0 - f_j) * (1.0 - v))
                         * (f_k * w + (1.0 - f_k) * (1.0 - w))
                         * c[index];
+                }
+            }
+        }
+        accum
+    }
+
+    fn perlin_interp(c: &[Vec3], u: f64, v: f64, w: f64) -> f64 {
+        let uu = u * u * (3.0 - 2.0 * u);
+        let vv = v * v * (3.0 - 2.0 * v);
+        let ww = w * w * (3.0 - 2.0 * w);
+        let mut accum = 0.0;
+        let max = 2;
+        for i in 0..max {
+            let f_i = i as f64;
+            for j in 0..max {
+                let f_j = j as f64;
+                for k in 0..max {
+                    let f_k = k as f64;
+                    let weight_v = Vec3::new(u - f_i, v - f_j, w - f_k);
+                    let f_k = k as f64;
+                    let index = (i * max) + (j * max) + k;
+                    accum += (f_i * uu + (1.0 - f_i) * (1.0 - uu))
+                        * (f_j * vv + (1.0 - f_j) * (1.0 - vv))
+                        * (f_k * ww + (1.0 - f_k) * (1.0 - ww))
+                        * c[index].dot(&weight_v);
                 }
             }
         }
--- src/texture.rs
+++ src/texture.rs
@@ -51,7 +51,7 @@ impl Texture for CheckerTexture {
 impl Texture for NoiseTexture {
     fn value(&self, _u: f64, _v: f64, point: &Vec3) -> Color {
         let scaled_p = self.scale * *point;
-        Color::new(1.0, 1.0, 1.0) * self.noise.noise(&scaled_p)
+        Color::new(1.0, 1.0, 1.0) * 0.5 * (1.0 + self.noise.noise(&scaled_p))
     }
 }
 
