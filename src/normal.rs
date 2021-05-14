5fc75275eff0b0ae745bb8270ead1211301f2486
Jordan BRION <jrd.brion@gmail.com>
2021-05-14 14:41:06 +0200

    More Axis-Aligned Rectangles

--- src/normal.rs
+++ src/normal.rs
@@ -2,6 +2,16 @@ use super::ray::*;
 use super::vec3::*;
 
 pub trait NormalOp {
-    fn normal(&self, ray: &Ray, t: f64) -> (bool, Vec3);
+    fn normal(&self, ray: &Ray, t: f64) -> (bool, Vec3) {
+        let outward_normal = self.outward_normal(ray, t);
+        let front_face = ray.direction.dot(&outward_normal) < 0.0f64;
+        (
+            front_face,
+            match front_face {
+                true => outward_normal,
+                false => -outward_normal,
+            },
+        )
+    }
     fn outward_normal(&self, ray: &Ray, t: f64) -> Vec3;
 }
